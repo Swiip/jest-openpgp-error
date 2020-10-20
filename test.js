cb60a0373aaa395ef8e044a904da88b12b235801
Matthieu Lux <matthieu.lux@protonmail.com>
2020-10-20 10:09:46 +0200

    Workaround + new error

--- test.js
+++ test.js
@@ -1,9 +1,20 @@
+const textEncoding = require("text-encoding-utf-8");
+
+window.TextEncoder = textEncoding.TextEncoder;
+window.TextDecoder = textEncoding.TextDecoder;
+
 const openpgp = require("openpgp");
 
 describe("generate key", () => {
   it("should work", async () => {
-    const { publicKeyArmored, privateKeyArmored } = await openpgp.generateKey({
+    const keys = await openpgp.generateKey({
       userIds: [{ name: "Me", email: "me@mydomain.com" }],
+    });
+    const message = openpgp.message.fromText("test");
+    const result = await openpgp.encrypt({
+      message,
+      publicKeys: keys.key,
+      compression: openpgp.enums.compression.zip,
     });
   });
 });
